cdae860677dfd418fd9d0bf03ade300c84164c7f
devillierscbe <109145209+devillierscbe@users.noreply.github.com>
2023-05-10 14:42:13 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -1,4 +1,4 @@
 def tomcatDeploy()
 {
-  sh 'ansible-playbook libdemo.yml'
+ ansiblePlaybook credentialsId: 'Ansible', disableHostKeyChecking: true, installation: 'ansible', inventory: 'demo.inv', playbook: 'libdemo.yml'
 }
